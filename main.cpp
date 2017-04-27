fbdc0656852710cd42903c7417def7f7dbe1e705
Mirna Wahab <mirnaiwahab@gmail.com>
2017-04-27 11:04:23 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  Assignment2
+//  Double_Auction_Simulation_System
 //
 //  Created by Mirna Wahab on 29/09/2016.
 //  Copyright © 2016 Mirna Wahab. All rights reserved.
@@ -21,8 +21,6 @@ using namespace std;
 
 const double MINPRICE = 50;
 const double MAXPRICE = 150;
-//const int MINQUANTITY = 1;
-//const int MAXQUANTITY = 50;
 const int NUMBUYERS = 10;
 const int NUMSELLERS = 10;
 
@@ -37,55 +35,6 @@ const int NUMSELLERS = 10;
 int main()
 {
     srand((int)time(0));
-    
-/*    //Testing of classes:
- 
-    cout << "From Bid class:" << endl;
-    Bid bid("Mirna", 1, 'B', 30.00, 1);
-    bid.printBid();
-    
-
-    cout << "\n\nFrom Trader class:" << endl;
-    Trader trader("Mirna", 'B');
-    trader.printTrader();
-    Bid b = trader.generateBid();
-    b.printBid();
-
-    string tn;
-    cout << "\n\nFrom Buyer class: " << endl;
-    Buyer buyer("Buyer");
-    buyer.printTrader();
- 
-    cout << "From Seller class: " << endl;
-    Seller seller("Seller");
-    seller.printTrader();
- 
-    cout << "From Auctioneer class: \n" << endl;
-    Auctioneer auctioneer;
-    
-    tn = "Buyer";
-    for (int i; i < 10 ; i++)
-    {
-        Buyer buyer(tn + to_string(i));
-        auctioneer.recieveBid(buyer.generateBid());
-        
-    };
-
-     tn = "Seller";
-    for (int i; i < 10 ; i++)
-    {
-        Seller seller(tn + to_string(i));
-        auctioneer.recieveBid(seller.generateBid());
-        //seller.generateBid().printBid();
-    }
-    auctioneer.printRecievedBid();
-    
-    auctioneer.matching();
-    
-    //Match m(Bid a, Bid b);
-    //m.getMatches();
-*/
-
     
     Simulator simulator;
     simulator.simulate();
